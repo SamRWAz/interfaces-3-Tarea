a8261cb94e38e7d51e752e09329fa5d697bf9687
SamRWAz <sam.burbano1@gmail.com>
2026-03-10 20:23:50 -0500

    Importar archivos de servicios y tests de la semana 7

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+
+import { UserModule } from './user/user.module';
+import { RoleModule } from './role/role.module';
+
+@Module({
+    providers: [],
+    imports: [UserModule, RoleModule],
+})
+export class AuthModule {}
---
+++ src/games/game/game.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { UserModule } from '@/auth/user/user.module';
+
+import { Game } from '../entities/game.entity';
+
+import { GameService } from './game.service';
+import { GameController } from './game.controller';
+
+@Module({
+    providers: [GameService],
+    imports: [UserModule, TypeOrmModule.forFeature([Game])],
+    controllers: [GameController],
+})
+export class GameModule {}
